1c7d4e1f227b8d38d04701ce202009c0c28ea78b
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-12 01:57:30 -0500

    Movement added to platform

--- LudumDare/Assets/Scripts/PlatformManagement.cs
+++ LudumDare/Assets/Scripts/PlatformManagement.cs
@@ -2,14 +2,66 @@
 using System.Collections;
 
 public class PlatformManagement : MonoBehaviour {
+    public const int RIGHT = 0;
+    public const int LEFT = 1;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Transform[] ropeTransforms;
+    public Transform[] ropePositions;
+    public float ropeSpeed = 15;
+
+    int[] currentPositions;
+    bool[] moveUp;
+
+    void Start()
+    {
+        moveUp = new bool[ropeTransforms.Length];
+        currentPositions = new int[ropePositions.Length];
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("LeftButton"))
+        {
+            moveRope(LEFT);
+        }
+        if (Input.GetButtonDown("RightButton"))
+        {
+            moveRope(RIGHT);
+        }
+
+        updateRopePosition(RIGHT);
+        updateRopePosition(LEFT);
+    }
+
+    void updateRopePosition(int id)
+    {
+        Transform rope = ropeTransforms[id];
+        Vector3 current =rope.position;
+        Vector3 goal = new Vector3(current.x, ropePositions[currentPositions[id]].position.y, current.z);
+        rope.position = Vector3.MoveTowards(rope.position, goal, Time.deltaTime * ropeSpeed);
+    }
+
+    void moveRope(int id)
+    {
+        
+        int currentPos = currentPositions[id];
+        if (moveUp[id] && currentPos <= 0)
+        {
+            moveUp[id] = false;
+        }
+        else if (!moveUp[id] && currentPos >= ropePositions.Length - 1)
+        {
+            moveUp[id] = true;
+        }
+
+        if (moveUp[id])
+        {
+            currentPositions[id]--;
+        }
+        else
+        {
+            currentPositions[id]++;
+        }
+    }
 }
