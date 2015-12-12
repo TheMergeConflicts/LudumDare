2debf86377309de10a77679198343373deacc8c2
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-12 10:38:09 -0500

    Spawner ready!

---
+++ LudumDare/Assets/Scripts/MinionSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionSpawner : MonoBehaviour {
+    public GameObject[] minions;
+    public float respawnTimeMin;
+    public float respawnTimeMax;
+    public Vector2 direction;
+
+    float respawnTimer;
+    
+    void Start()
+    {
+        restartTimer();
+    }
+
+    void Update()
+    {
+        respawnTimer = Mathf.MoveTowards(respawnTimer, 0, Time.deltaTime);
+        if (respawnTimer <= 0)
+        {
+            restartTimer();
+            createMinion();
+        }
+
+    }
+
+    void createMinion()
+    {
+        GameObject obj = (GameObject)Instantiate(minions[Random.Range(0, minions.Length)], transform.position, new Quaternion());
+        obj.GetComponent<MinionStats>().direction = direction;
+    }
+
+
+    void restartTimer()
+    {
+        respawnTimer = Random.Range(respawnTimeMin, respawnTimeMax);
+
+    }
+}
--- LudumDare/Assets/Scripts/MinionStats.cs
+++ LudumDare/Assets/Scripts/MinionStats.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class MinionStats : MonoBehaviour {
+    public string name;
     public Vector2 direction;
     public int healthPoints;
 
