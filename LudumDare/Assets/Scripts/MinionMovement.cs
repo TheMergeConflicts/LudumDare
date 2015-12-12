7ae16f0c982a5f3b53d1d6682e18c5b775b4fbe6
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-12 09:46:12 -0500

    First Minion created

---
+++ LudumDare/Assets/Scripts/MinionMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionMovement : MonoBehaviour {
+    Rigidbody2D rigid;
+    MinionStats stats;
+
+    void Start()
+    {
+        stats = GetComponent<MinionStats>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rigid.velocity = stats.direction * stats.speed + Vector2.up * rigid.velocity.y;
+
+    }
+
+
+}
---
+++ LudumDare/Assets/Scripts/MinionStats.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionStats : MonoBehaviour {
+    public Vector2 direction;
+    public float speed;
+    public int healthPoints;
+
+
+}
