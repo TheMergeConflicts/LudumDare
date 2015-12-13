73a05384a4561ec446c389a36b54c31604a105ac
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-13 11:12:10 -0500

    Ages now increase

--- LudumDare/Assets/Scripts/PlayerStats.cs
+++ LudumDare/Assets/Scripts/PlayerStats.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class PlayerStats : MonoBehaviour {
     public int age;
     public float health;
-    public float yearTime;
+    public float yearTime = 5;
 
+
+    float maxHealth;
     float ageTimer;
 
+    void Start()
+    {
+        this.maxHealth = this.health;
+        ageTimer = yearTime;
+
+    }
 
     void Update()
     {
         health = Mathf.MoveTowards(health, 0, Time.deltaTime);
+        updateAge();
     }
 
     void updateAge ()
@@ -19,7 +28,18 @@ public class PlayerStats : MonoBehaviour {
         ageTimer = Mathf.MoveTowards(ageTimer, 0, Time.deltaTime);
         if (ageTimer <= 0)
         {
+            ageTimer = yearTime;
+            age++;
+        }
 
+    }
+
+    public void updateHealth(float health)
+    {
+        this.health += health;
+        if (this.health > 100)
+        {
+            this.health = 100;
         }
     }
 }
