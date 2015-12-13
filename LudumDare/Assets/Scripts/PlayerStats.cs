f48bcf9bcee27d7a319d623958c3e713663f7270
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-13 15:37:15 -0500

    Difficulty now increases as time goes by

--- LudumDare/Assets/Scripts/PlayerStats.cs
+++ LudumDare/Assets/Scripts/PlayerStats.cs
@@ -9,17 +9,20 @@ public class PlayerStats : MonoBehaviour {
 
     float maxHealth;
     float ageTimer;
+    float deteriationRate = .5f;
+    MinionSpawner spawner;
 
     void Start()
     {
         this.maxHealth = this.health;
         ageTimer = yearTime;
-
+        spawner = GameObject.FindGameObjectWithTag("Spawner").GetComponent<MinionSpawner>();
     }
 
     void Update()
     {
-        health = Mathf.MoveTowards(health, 0, Time.deltaTime);
+        print(health);
+        health = Mathf.MoveTowards(health, 0, Time.deltaTime * deteriationRate);
         updateAge();
     }
 
@@ -30,6 +33,8 @@ public class PlayerStats : MonoBehaviour {
         {
             ageTimer = yearTime;
             age++;
+            spawner.increaseSpawnRate();
+            deteriationRate *= 1.03f;
         }
 
     }
