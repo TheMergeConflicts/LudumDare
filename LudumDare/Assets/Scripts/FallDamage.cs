6f5e5ec2948c2c9bd9e79359f4a16edf9fab8229
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-12 21:28:47 -0500

    fall damage added to game

---
+++ LudumDare/Assets/Scripts/FallDamage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallDamage : MonoBehaviour {
+    public float lethalHeight;
+    public Vector3 positionBeforeFall;
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        positionBeforeFall = transform.position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        float checkFallDamage = Mathf.Abs(transform.position.y - positionBeforeFall.y);
+        if (checkFallDamage > 6.1f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
