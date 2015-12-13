e1f1f53124d47d5555a2c5fa69d1dea3c493da6b
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-13 11:01:34 -0500

    fixed fill for health bar

--- LudumDare/Assets/Scripts/PlayerStats.cs
+++ LudumDare/Assets/Scripts/PlayerStats.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class PlayerStats : MonoBehaviour {
     public int age;
     public float health;
-    
+    public float yearTime;
+
+    float ageTimer;
 
 
     void Update()
     {
         health = Mathf.MoveTowards(health, 0, Time.deltaTime);
+    }
+
+    void updateAge ()
+    {
+        ageTimer = Mathf.MoveTowards(ageTimer, 0, Time.deltaTime);
+        if (ageTimer <= 0)
+        {
+
+        }
     }
 }
