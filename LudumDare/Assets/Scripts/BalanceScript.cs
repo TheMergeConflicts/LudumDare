92ad30b2aead170e13a2a7b184159016419a0ac8
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-01-12 16:22:37 -0500

    Balance Script done. Need to apply

--- LudumDare/Assets/Scripts/BalanceScript.cs
+++ LudumDare/Assets/Scripts/BalanceScript.cs
@@ -2,11 +2,73 @@
 using System.Collections;
 
 public class BalanceScript : MonoBehaviour {
-    public float[] weightChance;
+    public const int RED = 0;
+    public const int GREEN = 1;
+    public const int YELLOW = 2;
+    public const int BLUE = 3;
+
+
+    public float[] weightChance = new float[8];
+    int lastRoll;
+
+    /// <summary>
+    /// This method will adjust the likelyhood that a spawner of a certain color will spawn a minion.
+    /// </summary>
+    /// <param name="scale"></param>
+    /// <param name="color"></param>
+    public void adjustSpawnerWeight(float scale, int color)
+    {
+        weightChance[color * 2] *= scale;
+        weightChance[color * 2 + 1] *= scale;
+        normalizeChance();
+    }
+
+    /// <summary>
+    /// This method is used for to adjust the likelyhood that a minion of a certain color will respawn
+    /// </summary>
+    /// <param name="scale"></param>
+    /// <param name="color"></param>
+    public void adjustMinionSpawnWeight(float scale, int color)
+    {
+        int changeSlot = color / 2;
+        if (color % 2 == 0)
+        {
+            changeSlot += 2;
+        }
+        weightChance[changeSlot] *= scale;
+        weightChance[changeSlot + 4] *= scale;
+        normalizeChance();
+    }
 
     public int rollChance()
     {
+        float randVal = Random.Range(0f, 1f);
+        float min = 0;
+        float max = weightChance[0];
+        int i = 0;
+        foreach(float f in weightChance)
+        {
+            if (randVal >= min && randVal <= max)
+            {
+                lastRoll = i;
+                return i;
+            }
+            i++;
+            min = max;
+            max = weightChance[i];
+        }
+        lastRoll = -1;
         return -1;
+    }
+
+    public int getColorSpawn()
+    {
+        return lastRoll / 2;
+    }
+
+    public bool getMinionUp()
+    {
+        return lastRoll % 2 == 0;
     }
 
     public void resetChance()
